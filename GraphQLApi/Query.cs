e9663c9bd5ac19d2d6429d6a381d5394803d4015
Pavel Klos <pavelkloscz@gmail.com>
2023-06-20 07:52:54 +0200

    Creating Hot Chocolate 13 GraphQL API

---
+++ GraphQLApi/Query.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphQLApi;
+
+public class Query
+{
+    public Book GetBook() =>
+        new("C# in Depth", new Author("Jon Skeet"));
+
+    public IEnumerable<Book> GetBooks()
+    {
+        yield return new("C# in Depth", new Author("Jon Skeet"));
+        yield return new("C# in Depth - 2nd Edition", new Author("Jon Skeet"));
+        yield return new("C# in Depth - 3rd Edition", new Author("Jon Skeet"));
+    }
+}
+
+public record Book(string Title, Author Author);
+
+public record Author(string Name);
