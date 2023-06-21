6653bfb122d9be6192b9d2b4b7a48364e4e1d28e
Pavel Klos <pavelkloscz@gmail.com>
2023-06-21 16:33:37 +0200

    Extending Types in Hot Chocolate 13 GraphQL API

---
+++ GraphQLApi/AuthorType.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphQLApi;
+
+public class AuthorType : ObjectType<Author>
+{
+    protected override void Configure(IObjectTypeDescriptor<Author> descriptor)
+    {
+        // Add Field (get all books for Author.Id
+        descriptor.Field("booksAdded")
+            .Resolve(context =>
+            {
+                var id = context.Parent<Author>().Id;
+
+                return context.Service<ApplicationDbContext>().Books.Where(book => book.AuthorId == id);
+            })
+            .Serial()
+            .Type<NonNullType<ListType<BookType>>>();
+    }
+}
--- GraphQLApi/BookType.cs
+++ GraphQLApi/BookType.cs
@@ -6,6 +6,35 @@ public class BookType : ObjectType<Book>
     {
         //descriptor.Field(book => book.AuthorId).IsProjected(); // true = default
         //descriptor.Field(book => book.AuthorId).IsProjected(false);
-        descriptor.Field(book => book.AuthorId);
+
+        // Ignore Field
+        // - The field `authorId` does not exist on the type `Book`.
+        //descriptor.Field(book => book.AuthorId).Ignore();
+
+        // Add Field
+        //descriptor.Field("authorAdded")
+        //    .Resolve(async context =>
+        //    {
+        //        var keyValues = new object[] { context.Parent<Book>().AuthorId };
+        //        var cancellationToken = context.RequestAborted;
+
+        //        return await context.Service<ApplicationDbContext>().Authors.FindAsync(keyValues, cancellationToken);
+        //    })
+        //    .Serial()
+        //    //.Parallel()
+        //    .Type<NonNullType<AuthorType>>();
+
+        // Replace Field
+        descriptor.Field(book => book.AuthorId)
+            .Name("authorReplaced")
+            .Resolve(async context =>
+            {
+                var keyValues = new object[] { context.Parent<Book>().AuthorId };
+                var cancellationToken = context.RequestAborted;
+
+                return await context.Service<ApplicationDbContext>().Authors.FindAsync(keyValues, cancellationToken);
+            })
+            .Serial()
+            .Type<NonNullType<AuthorType>>();
     }
 }
