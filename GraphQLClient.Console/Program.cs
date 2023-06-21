af7ae0e6e4c827dc2d024145b80e74811b5a6db2
Pavel Klos <pavelkloscz@gmail.com>
2023-06-21 20:15:53 +0200

    Generate and use ConferenceClient.Client.cs file

--- GraphQLClient.Console/Program.cs
+++ GraphQLClient.Console/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// [Get started with Strawberry Shake in Console application] TUTORIAL
+// - https://chillicream.com/docs/strawberryshake/v13/get-started/console
+// - FILE 'ConferenceClient.Client.cs' GENERATED
+// - IN FOLDER '\obj\Debug\net8.0\berry'
+
+using GraphQLClient.Console.GraphQL;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection
+    .AddConferenceClient()
+    .ConfigureHttpClient(client =>
+        client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
+
+IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+IConferenceClient client = services.GetRequiredService<IConferenceClient>();
+
+var result = await client.GetSessions.ExecuteAsync();
+result.EnsureNoErrors();
+
+foreach (var session in result.Data.Sessions.Nodes)
+{
+    Console.WriteLine(session.Title);
+}
