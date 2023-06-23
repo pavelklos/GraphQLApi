d37bb8cc0971e330834e95c8b931046d28a9492e
Pavel Klos <pavelkloscz@gmail.com>
2023-06-23 17:16:07 +0200

    Add CryptoClient to dependency injection and using namespaces

--- GraphQLClient.BlazorWebAssembly/Program.cs
+++ GraphQLClient.BlazorWebAssembly/Program.cs
@@ -8,4 +8,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Strawberry Shake generator produced Client, but also components that we can use in Blazor
+// - [CryptoClient.Client.cs] client generated in folder '\obj\Debug\net8.0\berry'
+//   - we can use extension AddCryptoClient() on IServiceCollection
+//   - add new CryptoClient to dependency injection
+builder.Services
+    .AddCryptoClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://demo.chillicream.com/graphql"));
+
 await builder.Build().RunAsync();
